affb7b4960673e51fa7c569117669cd386d27062
91973 <91973@host.docker.internal>
2022-09-04 03:12:59 +0530

    added cors security

--- src/main/java/com/football/league/security/APISecurityConfig.java
+++ src/main/java/com/football/league/security/APISecurityConfig.java
@@ -1,5 +1,7 @@
 package com.football.league.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,28 +17,57 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import com.football.league.client.FootballRestClient;
 
 @Configuration
+//@EnableWebSecurity(debug = true)
 @EnableWebSecurity
 @Order(1)
 public class APISecurityConfig {
 
+	private static Logger logger = LoggerFactory.getLogger(FootballRestClient.class);
+	
 	@Value("${football.http.auth-token-header-name}")
 	private String principalRequestHeader;
 
 	@Value("${football.http.auth-token}")
 	private String principalRequestValue;
 
-	
 	AuthenticationManager authenticationManager;
 
     @Autowired
     UserDetailsService userDetailsService;
-
   
     @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!principalRequestValue.equals(principal)) {
+                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        
+        http.antMatcher("/api/**").
+    		csrf().disable().
+    		cors().disable().
+            sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+            and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
+
+        return http.build();
+    }
+    
+    /*@Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
@@ -48,43 +79,21 @@ public class APISecurityConfig {
             .and()
             .authenticationManager(authenticationManager)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
         return http.build();
-    }
-   
-	/**
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
-        filter.setAuthenticationManager(new AuthenticationManager() {
-
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                String principal = (String) authentication.getPrincipal();
-                if (!principalRequestValue.equals(principal))
-                {
-                    throw new BadCredentialsException("The API key was not found or not the expected value.");
-                }
-                authentication.setAuthenticated(true);
-                return authentication;
-            }
-        });
-        http.antMatcher("/api/**").
-                csrf().disable().
-                cors().disable().
-                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-                and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
-
-        return http.build();
-    }**/
-	
-	@Bean
+    }*/
+    
+	/*@Bean
     WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*");
+            	registry.addMapping("/**")
+	                .allowedOrigins("*")
+	                .allowedMethods("*")
+	                .maxAge(3600L)
+	                .allowedHeaders("*");
             }
         };
-    }
+    }*/
 }
